4989e45ace5c949584cecba5ad08172245cf428f
Edward Deam <edwardcrdeam@gmail.com>
2020-04-28 19:37:59 -0700

    Add User to Restaurant relation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
+  belongs_to :user
+
   validates :name, presence: true, length: { minimum: 2 }
   validates :cuisine, length: { minimum: 2 }
   validates :location, length: { minimum: 2 }
