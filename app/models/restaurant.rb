91630b61ba311f715be16e2d3306265c2caf7aa8
Edward Deam <edwardcrdeam@gmail.com>
2020-04-29 03:12:42 -0700

    Add card to index

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,8 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2 }
   validates :cuisine, length: { minimum: 2 }
   validates :location, length: { minimum: 2 }
+
+  def short_description
+    self.description[..97] + '...'
+  end
 end
