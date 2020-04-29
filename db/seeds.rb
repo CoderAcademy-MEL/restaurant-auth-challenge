c152328ef4a204a27d644d8eef7beaa79ae36eb6
Edward Deam <edwardcrdeam@gmail.com>
2020-04-28 21:46:00 -0700

    Add Restaurant to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,31 @@
-# Cleanup
+# Cleanup - Order is important!
+Restaurant.destroy_all
 User.destroy_all
 
 # Create users
-User.create(email: 'admin@restaurant.com', password: 'password', admin: true)
-User.create(email: 'test-user1@restaurant.com', password: 'password')
-User.create(email: 'test-user2@restaurant.com', password: 'password')
+user_admin = User.create(email: 'admin@restaurant.com', password: 'password', admin: true)
+user_one = User.create(email: 'test-user1@restaurant.com', password: 'password')
+user_two = User.create(email: 'test-user2@restaurant.com', password: 'password')
+
+
+# Create restaurants
+4.times do
+    r = Restaurant.create(
+      name: Faker::Restaurant.name,
+      location: Faker::Address.state,
+      cuisine: Faker::Restaurant.type,
+      description: Faker::Restaurant.description,
+      user: user_one
+    )
+    puts "#{r.name} created!"
+    r = Restaurant.create(
+      name: Faker::Restaurant.name,
+      location: Faker::Address.state,
+      cuisine: Faker::Restaurant.type,
+      description: Faker::Restaurant.description,
+      user: user_two
+    )
+    puts "#{r.name} created!"
+end
+
 
