8013d6d6be1deb40947546b327024951dd1f51e2
Edward Deam <edwardcrdeam@gmail.com>
2020-04-29 02:26:19 -0700

    Add Active Storage

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :cuisine, length: { minimum: 2 }
