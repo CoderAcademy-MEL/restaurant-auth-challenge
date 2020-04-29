0170ffdadb7b3f088e31c7272a2eb0de9b70d943
Edward Deam <edwardcrdeam@gmail.com>
2020-04-29 04:21:03 -0700

    restaurant params image fix

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -43,7 +43,7 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :location, :cuisine, :description)
+    params.require(:restaurant).permit(:name, :location, :cuisine, :description, :image)
   end
 
   def find_restaurant
